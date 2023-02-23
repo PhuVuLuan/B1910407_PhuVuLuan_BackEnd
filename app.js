1946a4bcdaf96a11823a33c79421aef4623d7452
PhuVuLuan <phuvuluan010101@gmail.com>
2023-02-23 07:42:48 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -13,11 +13,14 @@ app.use("/api/contacts", contactsRouter);
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
-
-app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
+  
+  app.use((error, req, res, next) => {
+      res.status(error.statusCode || 500).json({
+        error: {
+          status: error.statusCode || 500,
+          message: error.message || 'Internal Server Error',
+        },
+      });
     });
-});
 
 module.exports = app;
